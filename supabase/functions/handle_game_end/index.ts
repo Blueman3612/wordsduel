88a3aaa72a9a75d753a22f38da93563a69d25eb1
Blueman3612 <nathan.hall87@gmail.com>
2025-02-21 16:17:45 -0600

    Update Supabase function imports to use GitHub raw content URL for CORS headers

--- supabase/functions/handle_game_end/index.ts
+++ supabase/functions/handle_game_end/index.ts
@@ -3,9 +3,9 @@
 // This enables autocomplete, go to definition, etc.
 
 // Setup type definitions for built-in Supabase Runtime APIs
-import { serve } from "jsr:@std/http@0.218.2/server"
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts"
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.1"
-import { corsHeaders } from '../_shared/cors.ts'
+import { corsHeaders } from "https://raw.githubusercontent.com/supabase/supabase/master/examples/edge-functions/supabase/functions/_shared/cors.ts"
 
 console.log("Hello from Functions!")
 
