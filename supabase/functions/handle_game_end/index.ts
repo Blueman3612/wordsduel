37a3dcc0c75e055f5a45a60f8e7c5c971530131d
Blueman3612 <nathan.hall87@gmail.com>
2025-02-21 16:11:09 -0600

    Update Deno and Supabase function imports to latest module specifications

--- supabase/functions/handle_game_end/index.ts
+++ supabase/functions/handle_game_end/index.ts
@@ -2,8 +2,10 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { serve } from "jsr:@std/http@0.218.2/server"
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
 import { corsHeaders } from '../_shared/cors.ts'
 
 console.log("Hello from Functions!")
